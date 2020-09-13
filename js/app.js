6ab4eabc0431d7d5bdc0c037dba0f135d044fc61
Nolan Poulin <nolanpster@gmail.com>
2020-09-12 17:28:55 -0700

    Center the map on downdown Seattle.

--- js/app.js
+++ js/app.js
@@ -108,7 +108,7 @@ mapboxgl.accessToken = MAPBOX_TOKEN;
 var map = new mapboxgl.Map({
   container: 'map',
   style: MAPBOX_STYLE,
-  center: [-74.005, 40.714],
+  center: [-122.353, 47.605],
   zoom: 13,
   maxZoom: 22,
   minZoom: 13,
